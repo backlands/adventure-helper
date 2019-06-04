f277ff8dd435059f728646ec350835727c4f9bd1
Eric Klempnauer <me@eric-klempnauer.com>
2019-06-03 20:45:09 -0600

    Remove PropTypes from Input for now

--- src/client/components/dumb/Input/index.jsx
+++ src/client/components/dumb/Input/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // eslint-disable-next-line object-curly-newline
 const Input = ({ name, label, text, type, id, value, handleChange }) => (
@@ -16,15 +15,5 @@ const Input = ({ name, label, text, type, id, value, handleChange }) => (
   </div>
 
 );
-
-Input.propTypes = {
-  name: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  id: PropTypes.string,
-  value: PropTypes.string,
-  handleChange: PropTypes.func.isRequired,
-};
 
 export default Input;
