44ca0ab14ed2251598355ade3de1859679b14be2
Eric Klempnauer <me@eric-klempnauer.com>
2019-06-06 19:04:21 -0600

    Fix ClassStat input types

--- src/client/components/smart/ClassStats/index.jsx
+++ src/client/components/smart/ClassStats/index.jsx
@@ -24,37 +24,37 @@ class Skill extends React.PureComponent {
           value={this.props.bab}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='skillPoints'
           id={this.props.id}
           value={this.props.skillPoints}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='favouredBonus'
           id={this.props.id}
           value={this.props.favouredBonus}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='fortitudeBonus'
           id={this.props.id}
           value={this.props.fortitudeBonus}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='reflexBonus'
           id={this.props.id}
           value={this.props.reflexBonus}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='willBonus'
           id={this.props.id}
           value={this.props.willBonus}
           handleChange={this.props.handleChange} />
         <Input
-          type='text'
+          type='number'
           name='levels'
           id={this.props.id}
           value={this.props.levels}
