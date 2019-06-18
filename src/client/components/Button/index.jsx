98dd98377b4f8d3499e35f49fd605d7f0a74136f
Eric Klempnauer <me@eric-klempnauer.com>
2019-06-17 20:39:58 -0600

    Global button styling

--- src/client/components/Button/index.jsx
+++ src/client/components/Button/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles.scss';
 
 // eslint-disable-next-line object-curly-newline
 const Button = ({ children, id, value, handleClick }) => (
 
-    <button id={id} value={value} onClick={handleClick}>
+    <button className='Button' id={id} value={value} onClick={handleClick}>
       {children}
     </button>
 
